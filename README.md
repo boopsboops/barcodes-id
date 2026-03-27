9ddab83f4a18efc28de4bd51f003b73170629244
rupert@sorubim <rupertcollins@gmail.com>
2026-03-27 11:02:08 +0000

    done process

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # barcodes-id
 taxonomic identification of DNA barcodes
 
-```sh
+### Identification of cox1 sequences
+
+```bash
 # download bold distilled database for blast and sintax and upzip
 wget https://us-sea-1.linodeobjects.com/boldistilled/blast.zip -O temp/bold-distilled-blast.zip
 wget https://us-sea-1.linodeobjects.com/boldistilled/sintax.zip -O temp/bold-distilled-sintax.zip
-
 wget https://us-sea-1.linodeobjects.com/boldistilled/source.zip -O temp/bold-distilled-source.zip
-
 unzip temp/bold-distilled-blast.zip -d temp
 unzip temp/bold-distilled-sintax.zip -d temp
 unzip temp/bold-distilled-source.zip -d temp
@@ -15,17 +15,32 @@ unzip temp/bold-distilled-source.zip -d temp
 # run blastn v2.14 local on bold
 blastn -task blastn -num_threads 8 -evalue 0.1 -word_size 11 -max_target_seqs 50 -db temp/blast/BOLDistilled_COI_Jan2026_SEQUENCES.fasta -outfmt "6 qseqid sseqid evalue length pident nident bitscore" -out temp/cox1-blast-local.out -query temp/cox1.fasta
 
-# run blast remote
-blastn -task blastn -remote -db core_nt -evalue 0.1 -word_size 11 -max_target_seqs 50 -outfmt "6 qseqid sgi evalue length pident nident bitscore" -out temp/cox1-blast-remote.out -query temp/cox1.fasta
-
-
 # run sintax
 vsearch --threads 8 --sintax temp/cox1.fasta --db temp/sintax/BOLDistilled_COI_Jan2026_SEQUENCES_sintax.fasta --sintax_cutoff 0.7 --tabbedout temp/cox1-sintax-out.tsv
 
+# process results
+scripts/cox1-process-results.R
+```
+
+
+### Identification of 16S sequences
+
+```bash
+wget https://www.reference-midori.info/download/Databases/GenBank269_2025-12-09/SINTAX/uniq/MIDORI2_UNIQ_NUC_GB269_lrRNA_SINTAX.fasta.gz -O temp/MIDORI2_UNIQ_NUC_GB269_lrRNA_SINTAX.fasta.gz
+wget https://www.reference-midori.info/download/Databases/GenBank269_2025-12-09/BLAST/uniq/MIDORI2_UNIQ_NUC_GB269_lrRNA_BLAST.zip -O temp/MIDORI2_UNIQ_NUC_GB269_lrRNA_BLAST.zip
+
+unzip temp/MIDORI2_UNIQ_NUC_GB269_lrRNA_BLAST.zip -d temp
+gzip -d -k temp/MIDORI2_UNIQ_NUC_GB269_lrRNA_SINTAX.fasta.gz 
+
+
+# run blastn v2.14 local on midori
+blastn -task blastn -num_threads 4 -evalue 0.1 -word_size 11 -max_target_seqs 50 -db temp/MIDORI2_UNIQ_NUC_GB269_lrRNA_BLAST -outfmt "6 qseqid sseqid evalue length pident nident bitscore" -out temp/16s-blast-local.out -query temp/16s.fasta
+
+# run sintax
+vsearch --threads 4 --sintax temp/16s.fasta --db temp/MIDORI2_UNIQ_NUC_GB269_lrRNA_SINTAX.fasta --sintax_cutoff 0.7 --tabbedout temp/16s-sintax-out.tsv
 
 # process results
-scripts/process-results.R
-
+scripts/16s-process-results.R
 
 
 ```
--- scripts/process-results.R
+++ scripts/process-results.R
@@ -6,6 +6,8 @@ library("here")
 library("tidyverse")
 library("glue")
 
+# load sample codes
+sample.codes <- readr::read_tsv(here::here("temp/sample-codes.tsv"), col_types=list("c","c"), show_col_types=FALSE)
 
 # make cols
 blast.cols <- list(
@@ -26,15 +28,38 @@ col.types <- do.call(cols, blast.cols)
 blast.results <- readr::read_tsv(here::here("temp/cox1-blast-local.out"), col_names=col.names, col_types=col.types, show_col_types=FALSE) |>
     tidyr::separate_wider_delim(dbid,delim="|",names=c("processid","binid"))
 
-
-# taxonomy
+# load bold taxonomy
 bold.tax <- readr::read_tsv(here::here("temp/source/BOLDistilled_COI_Jan2026_TAXONOMY.tsv"),show_col_types=FALSE) |> 
     dplyr::select(bin,family,species) |> 
+    dplyr::rename(blastid=species) |>
     dplyr::rename(binid=bin)
 
-blast.results |> dplyr::left_join(bold.tax) |> 
+# load sintax results
+sintax.cols <- c("sampleid","idsProbs","strand","ids")
+sintax.table <- readr::read_tsv(here::here("temp/cox1-sintax-out.tsv"),col_names=sintax.cols,show_col_types=FALSE) |>
+    tidyr::separate_wider_delim(cols=idsProbs,delim=",s:",names=c("higherid","speciesid")) |>
+    tidyr::separate_wider_delim(cols=speciesid,delim="(",names=c("sintaxid","sintaxBootstrap")) |> 
+    dplyr::mutate(sintaxBootstrap=stringr::str_replace_all(sintaxBootstrap,"\\)","")) |> 
+    dplyr::select(sampleid,sintaxid,sintaxBootstrap)
+
+# clean and filter blast resuts
+blast.results.clean <- blast.results |> dplyr::left_join(bold.tax) |> 
     dplyr::group_by(sampleid) |> 
     dplyr::slice_max(blastBitscore,with_ties=TRUE) |> 
     dplyr::ungroup() |>
-    dplyr::mutate(dateSearched=lubridate::today()) |>
-    print(n=Inf)
+    dplyr::mutate(dateSearched=lubridate::today())
+
+# annotate blast and join with sintax and sample ids
+blast.results.clean.ann <- blast.results.clean |> 
+    dplyr::left_join(sintax.table) |> 
+    dplyr::left_join(sample.codes) |>
+    dplyr::mutate(sampleid=glue::glue("{sampleid}-{sampleCode}")) |>
+    dplyr::mutate(identification=if_else(blastid==sintaxid,blastid,"no identification")) |>
+    dplyr::select(sampleid,processid,binid,family,identification,blastEvalue,blastLength,blastPident,sintaxBootstrap,dateSearched)
+
+# check and write out
+blast.results.clean.ann |> print(n=Inf)
+blast.results.clean.ann |> readr::write_csv(here::here("temp/cox1-id-results.csv"))
+
+# print table for report
+blast.results.clean.ann |> dplyr::mutate(output=glue::glue("{sampleid}_FishF1/R1: {identification} (blast length {blastLength}, identity {round(blastPident,digits=2)}, sintax bootstrap {sintaxBootstrap})")) |> pull(output)
